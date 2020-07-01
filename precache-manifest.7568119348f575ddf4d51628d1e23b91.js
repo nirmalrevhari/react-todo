14b6b671f43554d100ca107e59bc67abba1e20c1
Nirmal Kumar H <nirmal@appveen.com>
2020-07-02 00:24:15 +0530

    Updates

---
+++ precache-manifest.7568119348f575ddf4d51628d1e23b91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "255a56161bd49a3ad3a96cd9ae226ccc",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "78f96c44f9828e71a5cd",
+    "url": "/react-todo/static/css/main.05327e0c.chunk.css"
+  },
+  {
+    "revision": "e98bfeca000c41716f58",
+    "url": "/react-todo/static/js/2.ff4d8b35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.ff4d8b35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78f96c44f9828e71a5cd",
+    "url": "/react-todo/static/js/main.c452ac39.chunk.js"
+  },
+  {
+    "revision": "ce701484895442b2eb85",
+    "url": "/react-todo/static/js/runtime-main.e4e550a5.js"
+  }
+]);
